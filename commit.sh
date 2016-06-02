43a1b31112f8f8ea277f955518c89eb48929a882
phong <phong@phongs-MacBook-Pro.local>
2016-06-02 18:52:48 -0500

    Add auto commit

---
+++ commit.sh
@@ -0,0 +1,31 @@
+export THISDIR=$(pwd)
+
+echo "Updating .pldotfiles ..."
+for dir in \
+    .spacemacs.d \
+    .conf \
+    .hammerspoon \
+    .ssh/config \
+    .tmux.conf \
+    .zprezto \
+    .zpreztorc \
+    .zshrc 
+do
+    if [ -d $HOME/$dir ]
+    then
+        rm -rf $THISDIR/$dir
+        mkdir -p $THISDIR/$dir
+        cp -r $HOME/$dir/* $THISDIR/$dir
+    else
+        cp $HOME/$dir $THISDIR/$dir
+    fi
+done
+
+echo "Committing changes ..." && \
+    git status && \
+    git add -A && \
+    git commit -m "update" && \
+    git push
+
+echo "Done git commit and push!"
+ls -la
